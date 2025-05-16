6c5512bd8b78134bfba6abfb44eecf15e0baad3a
jgr121 <jg121@rice.edu>
2025-05-16 12:30:40 -0700

    feat(serving html): serve home html page

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,12 +2,35 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from Joshua!"))
+
+	files := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/pages/home.tmpl.html",
+	}
+
+	tp, err := template.ParseFiles(files...)
+	if err != nil {
+		http.Error(w,
+			http.StatusText(http.StatusInternalServerError),
+			http.StatusInternalServerError,
+		)
+		return
+	}
+
+	err = tp.Execute(w, nil)
+	if err != nil {
+		http.Error(w,
+			http.StatusText(http.StatusInternalServerError),
+			http.StatusInternalServerError,
+		)
+		return
+	}
 }
 
 func projectView(w http.ResponseWriter, r *http.Request) {
