c208cf68618048e84a78191afe36c9e4fa297e96
jgr121 <jg121@rice.edu>
2025-05-16 12:39:07 -0700

    feat(serving html): add create page html serve

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -64,7 +64,28 @@ func projectView(w http.ResponseWriter, r *http.Request) {
 }
 
 func projectCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display a form for creating a new project..."))
+	files := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/pages/create.tmpl.html",
+	}
+
+	tp, err := template.ParseFiles(files...)
+	if err != nil {
+		http.Error(w,
+			http.StatusText(http.StatusInternalServerError),
+			http.StatusInternalServerError,
+		)
+		return
+	}
+
+	err = tp.Execute(w, nil)
+	if err != nil {
+		http.Error(w,
+			http.StatusText(http.StatusInternalServerError),
+			http.StatusInternalServerError,
+		)
+		return
+	}
 }
 
 func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
