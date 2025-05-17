f179d11b4c97188e765a3e5a61377bbed0a2a57e
jgr121 <jg121@rice.edu>
2025-05-17 10:24:10 -0700

    refactor(serving html): extract rendering logic into template.go

--- internal/web/template.go
+++ internal/web/template.go
@@ -1,8 +1,32 @@
 package web
 
-import "github.com/jgr142/portfolio/internal/models"
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/jgr142/portfolio/internal/models"
+)
 
 type templateData struct {
 	Project  models.Project
 	Projects []models.Project
+}
+
+func (h *Handler) render(
+	w http.ResponseWriter,
+	r *http.Request,
+	files []string,
+	data templateData,
+) {
+	tp, err := template.ParseFiles(files...)
+	if err != nil {
+		h.serverError(w, r, err)
+		return
+	}
+
+	err = tp.Execute(w, data)
+	if err != nil {
+		h.serverError(w, r, err)
+		return
+	}
 }
