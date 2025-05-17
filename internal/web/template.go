b77cfd52841d4ade467cfdb640e032933c07a323
jgr121 <jg121@rice.edu>
2025-05-17 11:15:08 -0700

    feat(template cache): define template cache

--- internal/web/template.go
+++ internal/web/template.go
@@ -1,8 +1,11 @@
 package web
 
 import (
+	"errors"
 	"html/template"
 	"net/http"
+	"path/filepath"
+	"strings"
 
 	"github.com/jgr142/portfolio/internal/models"
 )
@@ -10,6 +13,10 @@ import (
 type templateData struct {
 	Project  models.Project
 	Projects []models.Project
+}
+
+type templateCache struct {
+	cache map[string]*template.Template
 }
 
 func (h *Handler) render(
@@ -29,4 +36,40 @@ func (h *Handler) render(
 		h.serverError(w, r, err)
 		return
 	}
+}
+
+func NewTemplateCache() (templateCache, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl.html")
+	if err != nil {
+		return templateCache{}, err
+	}
+
+	for _, page := range pages {
+		name := strings.TrimSuffix(filepath.Base(page), ".tmpl.html")
+
+		tp, err := template.ParseFiles("./ui/html/base.tmpl.html")
+		if err != nil {
+			return templateCache{}, err
+		}
+
+		tp, err = tp.ParseFiles(page)
+		if err != nil {
+			return templateCache{}, err
+		}
+
+		cache[name] = tp
+	}
+
+	return templateCache{cache: cache}, nil
+}
+
+func (tc *templateCache) Get(pageName string) (*template.Template, error) {
+	tp, ok := tc.cache[pageName]
+	if !ok {
+		return nil, errors.New("template does not exist")
+	}
+
+	return tp, nil
 }
