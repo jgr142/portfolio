5fc0d88b10bd8c2e0daa4e4c42355dc5c1a87c30
jgr121 <jg121@rice.edu>
2025-05-17 12:26:31 -0700

    feat(serving html): serve static files

--- internal/web/routes.go
+++ internal/web/routes.go
@@ -13,6 +13,10 @@ func InitMux(h Handler) http.Handler {
 
 func (r *Router) routes() http.Handler {
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("GET /{$}", r.h.home)
 	mux.HandleFunc("GET /project/view/{id}", r.h.projectView)
 	mux.HandleFunc("GET /project/create", r.h.projectCreate)
